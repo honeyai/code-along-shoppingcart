4b1f6aea410d3a2d1a55d67e68f88d2fb0e13d87
honeyai <contact.jessicasanchez@gmail.com>
2020-07-10 16:14:13 -0400

    Template

---
+++ src/Components/Shoppingcart.jsx
@@ -0,0 +1,42 @@
+//! If you have the extension what shortcut can you use to get the component we want.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================================================================================
+//TODO: Put this inside of your render() in the return() once you have deleted the divs that show up with with extension's shortcut: command + x >> Will erase what you have selected, storing it into the clip board and then command + v will paste it where your cursor is.
+
+<div className="wrapper">  //This is going to be the container of EVERYTHING
+  //Something to hold our items
+  <div className="items">
+
+    <div className="firstItem">
+    //How do we get this to show us our items?
+        //....
+
+    </div>
+    <div className="secondItem">
+        //....
+    </div>
+  </div>
+
+  //Something to hold our checkout
+  <div className="checkOut">
+
+    //What do we need in here/ how do we get it to show. 
+
+  </div>
+</div>
